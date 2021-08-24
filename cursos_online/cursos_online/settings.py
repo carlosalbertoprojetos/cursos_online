e9c918d76010ad6535db8436025aa869fab06d30
carlosalbertoprojetos <carlosalbertoprojetos2020@gmail.com>
2021-08-24 17:10:44 -0300

    arquivos estáticos

--- cursos_online/cursos_online/settings.py
+++ cursos_online/cursos_online/settings.py
@@ -124,6 +124,14 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATICFILES_DIRS = [
+    'static'
+]
+
+STATIC_ROOT = (
+    os.path.join(BASE_DIR, 'staticfiles')
+)
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
